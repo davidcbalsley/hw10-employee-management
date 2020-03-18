01a17a3a8c8a96843854cb6818597104252aa7ca
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-03-17 23:34:10 -0500

    connecting to database

--- employee-tracker.js
+++ employee-tracker.js
@@ -1,3 +1,22 @@
 var cTable = require("console.table");
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Port, if not 3306
+    port: 3306,
+  
+    // Username
+    user: "root",
+  
+    // Password
+    password: "",
+    database: "employee_trackerDB"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+  });
